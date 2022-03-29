d0888d9f2c85b08e20c1918806c300fbdb476d6a
LotusBlossom42 <36244638+LotusBlossom42@users.noreply.github.com>
2022-03-29 16:35:16 -0700

    shopping cart files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as ReactDOMClient from 'react-dom/client';
+
+// ReactDOM.render(<App/>,document.querySelector("#root"));
+
+import App from 'App';
+
+const container = document.getElementById('root');
+
+// Create *and* render a root with hydration.
+const root = ReactDOMClient.hydrateRoot(container, <App tab="home" />);
+// Unlike with createRoot, you don't need a separate root.render() call here
